a745c56d9681bbbb268fed0fac29c8df141b616a
MaxSC2 <155305256+MaxSC2@users.noreply.github.com>
2026-09-21 17:00:48 +0500

    fix: import Android AlarmClock from provider package

--- app/src/main/java/com/maxsc2/miniuna/AndroidTools.kt
+++ app/src/main/java/com/maxsc2/miniuna/AndroidTools.kt
@@ -1,6 +1,6 @@
 package com.maxsc2.miniuna
 
-import android.app.AlarmClock
+import android.provider.AlarmClock
 import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
