73f709bac130987609345cb3072eeb72347d35e3
MaxSC2 <155305256+MaxSC2@users.noreply.github.com>
2026-09-21 16:38:53 +0500

    fix: sync Cactus callback declarations

--- app/src/main/java/com/cactus/CactusCallbacks.kt
+++ app/src/main/java/com/cactus/CactusCallbacks.kt
@@ -3,3 +3,7 @@ package com.cactus
 fun interface CactusTokenCallback {
     fun onToken(token: String, tokenId: Int)
 }
+
+fun interface CactusLogCallback {
+    fun onLog(level: Int, component: String, message: String)
+}
